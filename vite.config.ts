0124d4eddc6d364b59be6200e52a5a37e931cb48
Iván López Hdez <ivanlhz@gmail.com>
2025-05-06 14:54:54 +0100

    chore: install tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),  tailwindcss()],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
