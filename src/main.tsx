ace1777d7e022ee993cb268ad3cd54e2c1b36987
Iván López Hdez <ivanlhz@gmail.com>
2025-05-07 12:01:25 +0100

    chore: move and small refactorizes

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
-import App from './App.tsx'
+import App from './app/App.tsx'
 import { ErrorBoundary } from 'react-error-boundary'
 import ErrorFallback from '@/ui/organisms/ErrorFallback'
 import './index.css'
